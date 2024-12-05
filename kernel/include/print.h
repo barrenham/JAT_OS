83162ba1025941b476ff9c0c53b98aa811ae0035
barrenham <2247572291@qq.com>
2024-12-05 15:45:49 +0800

    加入清屏

--- kernel/include/print.h
+++ kernel/include/print.h
@@ -5,4 +5,5 @@ void        put_char(uint8_t char_asci);
 void        put_string(char* message);
 void        put_int(int number);
 void        set_cursor(int position);
+void        clean_screen(void);
 #endif  
